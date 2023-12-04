e17d5bf9b58729074511b98e01e5bfa092fa8ac2
Bhanu <sandrabhanuprakash@gmail.com>
2023-12-04 23:19:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def abd(request):
+    return render(request,'abd.html')
+def trasation(request):
+    return render(request,'trasation.html')
